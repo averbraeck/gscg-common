4218a6f17a800c5aa814c33e6b6cc97940a74056
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-09-21 19:43:59 +0200

    Image utilities

---
+++ src/main/java/org/gscg/image/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Image utilities.
+ * <p>
+ * Copyright (c) 2025-2025 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-3-style license.
+ * </p>
+ * @author <a href="https://github.com/averbraeck">Alexander Verbraeck</a>
+ */
+package org.gscg.image;
